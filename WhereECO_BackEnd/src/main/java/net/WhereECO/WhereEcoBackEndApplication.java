f3c1f2879a153ced5bd3413d72eeb06332e6939e
김자경 <rlawkrud15@gmail.com>
2022-09-05 14:48:15 +0900

    Feat : 프로젝트 생성

---
+++ WhereECO_BackEnd/src/main/java/net/WhereECO/WhereEcoBackEndApplication.java
@@ -0,0 +1,13 @@
+package net.WhereECO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereEcoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereEcoBackEndApplication.class, args);
+	}
+
+}
---
+++ WhereECO_BackEnd/src/test/java/net/WhereECO/WhereEcoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package net.WhereECO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereEcoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
